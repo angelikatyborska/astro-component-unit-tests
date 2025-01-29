5059879ac72e50f3530202b451f6899c249ea933
Angelika Cathor <angelikacathor@fastmail.com>
2025-01-29 19:45:05 +0100

    Add simple Counter test

---
+++ src/components/Counter.test.ts
@@ -0,0 +1,26 @@
+// @vitest-environment happy-dom
+import { describe, test, expect } from 'vitest';
+import Counter from "./Counter.astro";
+import {renderAstroComponent} from "../test/helpers.ts";
+
+describe("Counter", () => {
+  test("has default initial count", async () => {
+    const result = await renderAstroComponent(Counter)
+    const currentCount = result.querySelector('[data-current-count]')
+
+    expect(currentCount).not.toBeNull();
+    expect(currentCount!.textContent).toEqual("0")
+  })
+
+  test("accepts a custom initial count", async () => {
+    const result = await renderAstroComponent(
+      Counter,
+      { props: { initialCount: "4" } }
+    );
+
+    const currentCount = result.querySelector('[data-current-count]')
+
+    expect(currentCount).not.toBeNull();
+    expect(currentCount!.textContent).toEqual("4")
+  })
+})
